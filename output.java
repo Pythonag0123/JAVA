08c8e6e9af8496d95892615a086b619201264ed7
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2024-06-30 12:10:27 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,12 @@
+package DAY2;
+
+public class output {
+    public static void main(String[] args){
+        //output
+        System.out.println("Hello World with java");
+        System.out.print("Hey it's khushi and i am starting my java journey, i will try my best to stay conistent with my coding and learning journey , thanks\n");
+
+        System.out.print("BYEBYE ,imma go and earn some money.");
+    }
+    
+}
---
+++ patternWithPrint.java
@@ -0,0 +1,8 @@
+package DAY2;
+
+public class patternWithPrint {
+    public static void main (String[] args){
+        System.out.println("*\n**\n***\n****");
+    }
+    
+}
