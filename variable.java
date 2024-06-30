16af102facca42e6b9a15a6ab0a96880964d9821
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2024-06-30 19:37:12 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,21 @@
+package DAY2;
+
+public class variable {
+    private static final boolean True = false;
+
+    public static void main(String[] args){
+        //variables sum
+       int a = 12;
+       int b= 10;
+       int sum = a+ b;
+       System.out.println(sum);
+       //difference
+       int diff= a-b;
+       System.out.println(diff);
+       //multiply
+       int mult =a*b;
+       System.out.println(mult);
+        
+    }
+    
+}
