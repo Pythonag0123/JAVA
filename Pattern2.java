cb10cde0ce7c00b1380f22890d8c27b5708abf20
Khushi Gangwar <146114877+Pythonag0123@users.noreply.github.com>
2024-07-02 16:38:12 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,24 @@
+package DAY4;
+import java.util.*;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int columns = sc.nextInt();
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0 || j == 0 || i == rows - 1 || j == columns - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); 
+        }
+        sc.close();
+    }
+}
